06596efd415213ab9b44928a8c734bece7125275
Liu Yang <zation1@gmail.com>
2016-06-06 13:28:17 +0800

    extract tranlated messages

--- src/index.js
+++ src/index.js
@@ -1,28 +1,67 @@
-import path from 'path';
-import { writeFileSync } from 'fs';
+import { join } from 'path';
+import { writeFileSync, readFileSync } from 'fs';
+import mapKeys from 'lodash/fp/mapKeys';
+import mapValues from 'lodash/fp/mapValues';
+import find from 'lodash/fp/find';
+import prop from 'lodash/fp/prop';
+import flow from 'lodash/fp/flow';
+import isEqual from 'lodash/fp/isEqual';
 
 class SimpleReactIntlWebpackPlugin {
-  constructor({ sourcePath, outputFile }) {
+  constructor({
+    sourcePath,
+    translationsPath,
+    outputPath,
+    methodName,
+    languages,
+    defaultLanguage,
+  }) {
     this.sourcePath = sourcePath;
-    this.outputFile = outputFile;
+    this.translationsPath = translationsPath;
+    this.outputPath = outputPath;
+    this.regExp = new RegExp(`${methodName || 'defineMessages'}\\(((.|\\n)*?)\\)`);
+    this.languages = languages;
+    this.defaultLanguage = defaultLanguage;
+    this.cachedDefaultMessages = null;
   }
 
   apply(compiler) {
-    compiler.plugin('emit', (compilation, callback) => {
-      const moduleMap = compilation.chunks[0].modules.filter(
-        ({ resource }) =>
-        resource
-        && resource.includes(this.sourcePath)
-        && !resource.includes('/node_modules/')
-        && resource.endsWith('.js')
-      ).map(module => ({
-        id: module.id,
-        file: path.relative(process.cwd(), module.resource),
-      }));
+    compiler.plugin('compilation', compilation => {
+      const translations = JSON.parse(readFileSync(this.translationsPath, 'utf8'));
 
-      writeFileSync(this.outputFile, JSON.stringify(moduleMap, null, 2));
+      compilation.plugin('record-modules', modules => {
+        const defaultMessages = modules.filter(({ resource }) =>
+          resource
+          && resource.includes(this.sourcePath)
+          && !resource.includes('/node_modules/')
+          && resource.endsWith('.js')
+        ).reduce((result, { resource, id }) => {
+          const source = readFileSync(resource, 'utf8');
+          const messagesMatches = this.regExp.exec(source);
+          if (messagesMatches && messagesMatches.length > 1) {
+            const messagesObject = eval(`(${messagesMatches[1].replace(/\n/g, '')})`);
+            result = {
+              ...result,
+              ...mapKeys(key => `${id}-${key}`, messagesObject),
+            };
+          }
+          return result;
+        }, {});
 
-      callback();
+        if (!isEqual(defaultMessages, this.cachedDefaultMessages)) {
+          this.languages.forEach(language => {
+            const messages = mapValues(value => flow(
+              find(translation => translation[this.defaultLanguage] === value),
+              prop(language)
+            )(translations), defaultMessages);
+            writeFileSync(
+              join(this.outputPath, `${language}-messages.js`),
+              `export default ${JSON.stringify(messages, null, 2)}`
+            );
+          });
+          this.cachedDefaultMessages = defaultMessages;
+        }
+      });
     });
   }
 }
